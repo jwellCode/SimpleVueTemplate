68ca9aa591e2d5d8e6ff51493f1700e1c1debe7d
jwellcode <123345x@googlemail.com>
2023-02-16 04:39:55 +0100

    update Kontakt component

--- src/components/Kontakt.vue
+++ src/components/Kontakt.vue
@@ -7,7 +7,7 @@
     amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
     nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
     diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-    clita kasd gubergren, nos sea takimata sanctus est Lorem ipsum dolor sit
+    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
     amet.
   </div>
 </template>
